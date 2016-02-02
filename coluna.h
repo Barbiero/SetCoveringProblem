c466e1ae4c7d050176c0526247c533dc7e956ee1
Pedro Barbiero <pedrobarbiero@gmail.com>
2016-02-02 08:32:12 -0200

    Codigo inicial

---
+++ coluna.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <cstdint>
+#include <unordered_set>
+#include <map>
+
+#include <iostream>
+
+typedef std::unordered_set<uint8_t> LinhaSet;
+
+
+
+class Coluna
+{
+
+public:
+	typedef std::map<uint16_t, Coluna*> ColunaArray;
+
+	Coluna(uint16_t _id, double _custo, LinhaSet _linhas) :
+		id(_id), custo(_custo), linhas(_linhas) {};
+	
+	uint16_t getId() { return id; }
+	double getCusto() { return custo; }
+
+	bool cobreLinha(uint8_t linha) {
+		return (linhas.find(linha) != linhas.cend());
+	}
+
+	void globalizar() {
+		Coluna::setColuna(this);
+	}
+
+	static void setColuna(Coluna* coluna){
+		getColunas().insert(
+			std::pair<uint16_t, Coluna*>(coluna->getId(), coluna));
+	}
+
+	static ColunaArray& getColunas() { 
+		static Coluna::ColunaArray colunas;
+		return colunas;
+	}
+	
+private:
+
+	/**
+	 * Identificador da coluna
+	 */
+	uint16_t id;
+
+	/**
+	 * Custo(ou peso) da coluna, menor = melhor
+	 */
+	double custo;
+
+	/**
+	 * Conjunto de linhas que a coluna cobre
+	 */
+	LinhaSet linhas;
+
+	/**
+	 * Lista persistente de colunas
+	 */
+	//static ColunaArray colunas;
+};
+
---
+++ ioColuna.cpp
@@ -0,0 +1,60 @@
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "coluna.h"
+
+int readLinhas(std::string filename)
+{
+	std::string tmp;
+	std::ifstream fin;
+	fin.open(filename);
+
+
+
+	//ler numero de linhas
+	int num_linhas;
+	fin >> tmp >> num_linhas;
+
+	//numero de colunas
+	int num_colunas;
+	fin >> tmp >> num_colunas;
+
+	//pula
+	fin >> tmp;
+
+	//colunas
+	std::string linhaAtual;
+	while(getline(fin, linhaAtual)){
+		int id_coluna = 0;
+		double custo = 0.0;
+		LinhaSet linhas;
+
+		std::stringstream linhaStream(linhaAtual);
+
+		linhaStream >> id_coluna >> custo;
+		if(id_coluna <= 0 || id_coluna > num_colunas) continue;
+		int linha_id;
+		while(linhaStream >> linha_id){
+			linhas.insert(linha_id);
+		}
+
+		Coluna* c = new Coluna(id_coluna, custo, linhas);
+		c->globalizar();
+	}
+
+	return Coluna::getColunas().size();
+}
+
+int main(int argv, char* argc[])
+{
+	if(argv < 2){
+		std::cout << "Digite o nome do arquivo" << std::endl;
+		return 0;
+	}
+
+	std::cout << readLinhas(argc[1]) << std::endl;
+
+	return 0;
+}
+
