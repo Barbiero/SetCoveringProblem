1a2771cb3ffc87b67c87e5e396851ddf0be467d6
Pedro Barbiero <pedrobarbiero@gmail.com>
2016-02-02 09:17:11 -0200

    tab ajustado para 4 espaços

--- src/coluna.h
+++ src/coluna.h
@@ -14,52 +14,52 @@ class Coluna
 {
 
 public:
-	typedef std::map<uint16_t, Coluna*> ColunaArray;
+    typedef std::map<uint16_t, Coluna*> ColunaArray;
 
-	Coluna(uint16_t _id, double _custo, LinhaSet _linhas) :
-		id(_id), custo(_custo), linhas(_linhas) {};
-	
-	uint16_t getId() { return id; }
-	double getCusto() { return custo; }
+    Coluna(uint16_t _id, double _custo, LinhaSet _linhas) :
+        id(_id), custo(_custo), linhas(_linhas) {};
+    
+    uint16_t getId() { return id; }
+    double getCusto() { return custo; }
 
-	bool cobreLinha(uint8_t linha) {
-		return (linhas.find(linha) != linhas.cend());
-	}
+    bool cobreLinha(uint8_t linha) {
+        return (linhas.find(linha) != linhas.cend());
+    }
 
-	void globalizar() {
-		Coluna::setColuna(this);
-	}
+    void globalizar() {
+        Coluna::setColuna(this);
+    }
 
-	static void setColuna(Coluna* coluna){
-		getColunas().insert(
-			std::pair<uint16_t, Coluna*>(coluna->getId(), coluna));
-	}
+    static void setColuna(Coluna* coluna){
+        getColunas().insert(
+            std::pair<uint16_t, Coluna*>(coluna->getId(), coluna));
+    }
 
-	static ColunaArray& getColunas() { 
-		static Coluna::ColunaArray colunas;
-		return colunas;
-	}
-	
+    static ColunaArray& getColunas() { 
+        static Coluna::ColunaArray colunas;
+        return colunas;
+    }
+    
 private:
 
-	/**
-	 * Identificador da coluna
-	 */
-	uint16_t id;
+    /**
+     * Identificador da coluna
+     */
+    uint16_t id;
 
-	/**
-	 * Custo(ou peso) da coluna, menor = melhor
-	 */
-	double custo;
+    /**
+     * Custo(ou peso) da coluna, menor = melhor
+     */
+    double custo;
 
-	/**
-	 * Conjunto de linhas que a coluna cobre
-	 */
-	LinhaSet linhas;
+    /**
+     * Conjunto de linhas que a coluna cobre
+     */
+    LinhaSet linhas;
 
-	/**
-	 * Lista persistente de colunas
-	 */
-	//static ColunaArray colunas;
+    /**
+     * Lista persistente de colunas
+     */
+    //static ColunaArray colunas;
 };
 
--- src/ioColuna.cpp
+++ src/ioColuna.cpp
@@ -6,55 +6,55 @@
 
 int readLinhas(std::string filename)
 {
-	std::string tmp;
-	std::ifstream fin;
-	fin.open(filename);
+    std::string tmp;
+    std::ifstream fin;
+    fin.open(filename);
 
 
 
-	//ler numero de linhas
-	int num_linhas;
-	fin >> tmp >> num_linhas;
+    //ler numero de linhas
+    int num_linhas;
+    fin >> tmp >> num_linhas;
 
-	//numero de colunas
-	int num_colunas;
-	fin >> tmp >> num_colunas;
+    //numero de colunas
+    int num_colunas;
+    fin >> tmp >> num_colunas;
 
-	//pula
-	fin >> tmp;
+    //pula
+    fin >> tmp;
 
-	//colunas
-	std::string linhaAtual;
-	while(getline(fin, linhaAtual)){
-		int id_coluna = 0;
-		double custo = 0.0;
-		LinhaSet linhas;
+    //colunas
+    std::string linhaAtual;
+    while(getline(fin, linhaAtual)){
+        int id_coluna = 0;
+        double custo = 0.0;
+        LinhaSet linhas;
 
-		std::stringstream linhaStream(linhaAtual);
+        std::stringstream linhaStream(linhaAtual);
 
-		linhaStream >> id_coluna >> custo;
-		if(id_coluna <= 0 || id_coluna > num_colunas) continue;
-		int linha_id;
-		while(linhaStream >> linha_id){
-			linhas.insert(linha_id);
-		}
+        linhaStream >> id_coluna >> custo;
+        if(id_coluna <= 0 || id_coluna > num_colunas) continue;
+        int linha_id;
+        while(linhaStream >> linha_id){
+            linhas.insert(linha_id);
+        }
 
-		Coluna* c = new Coluna(id_coluna, custo, linhas);
-		c->globalizar();
-	}
+        Coluna* c = new Coluna(id_coluna, custo, linhas);
+        c->globalizar();
+    }
 
-	return Coluna::getColunas().size();
+    return Coluna::getColunas().size();
 }
 
 int main(int argv, char* argc[])
 {
-	if(argv < 2){
-		std::cout << "Digite o nome do arquivo" << std::endl;
-		return 0;
-	}
+    if(argv < 2){
+        std::cout << "Digite o nome do arquivo" << std::endl;
+        return 0;
+    }
 
-	std::cout << readLinhas(argc[1]) << std::endl;
+    std::cout << readLinhas(argc[1]) << std::endl;
 
-	return 0;
+    return 0;
 }
 
